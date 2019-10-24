52c929385080df0554fada8beb9060b3f647847f
carsonwong0712 <cwong@pfprobotics.com>
2019-10-24 14:57:31 +1100

    blur user self-choosed image

--- app/src/main/java/com/example/background/workers/BlurWorker.java
+++ app/src/main/java/com/example/background/workers/BlurWorker.java
@@ -1,13 +1,16 @@
 package com.example.background.workers;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.text.TextUtils;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.work.Data;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
@@ -28,11 +31,20 @@ public class BlurWorker extends Worker {
     @Override
     public Result doWork() {
         Context applicationContext = getApplicationContext();
+        String resourceUri = getInputData().getString(Constants.KEY_IMAGE_URI);
 
         try {
-            Bitmap picture = BitmapFactory.decodeResource(applicationContext.getResources(), R.drawable.test);
+//            Bitmap picture = BitmapFactory.decodeResource(applicationContext.getResources(), R.drawable.test);
 
-            String resourceUri = getInputData().getString(Constants.KEY_IMAGE_URI);
+            if (TextUtils.isEmpty(resourceUri)) {
+                Log.e(TAG, "Invalid input uri");
+                throw new IllegalArgumentException("Invalid input uri");
+            }
+
+            ContentResolver resolver = applicationContext.getContentResolver();
+
+            //create the bitmap with user input uri
+            Bitmap picture = BitmapFactory.decodeStream(resolver.openInputStream(Uri.parse(resourceUri)));
 
             //Blur the bitmap
             Bitmap output = WorkerUtils.blurBitmap(picture, applicationContext);
@@ -40,11 +52,15 @@ public class BlurWorker extends Worker {
             //Write bitmap to temp file
             Uri outputUri = WorkerUtils.writeBitmapToFile(applicationContext, output);
 
+            Data outputData = new Data.Builder()
+                    .putString(Constants.KEY_IMAGE_URI, outputUri.toString())
+                    .build();
+
             //make notification on notification bar
             WorkerUtils.makeStatusNotification("Output is " + outputUri.toString(), applicationContext);
 
             //if it is error free, return success
-            return Result.success();
+            return Result.success(outputData);
         } catch (Throwable throwable) {
             Log.e(TAG, "Error applying blur", throwable);
             return Result.failure();
